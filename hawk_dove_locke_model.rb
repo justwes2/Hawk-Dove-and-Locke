a4103921f0e3dfe5c8fbe07505eec0860cce3cdc
Wes <wes.coffay@gmail.com>
2017-02-22 21:25:56 -0500

    inital commit

---
+++ hawk_dove_locke_model.rb
@@ -0,0 +1,21 @@
+pop = ['hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke','hawk', 'dove', 'locke']
+player1 = pop.sample
+player2 = pop.sample
+
+if player1='hawk' && player2='dove'
+
+elsif player1='hawk' && player2='hawk'
+
+elsif player1='dove' && player2='dove'
+
+elsif player1='locke' && player2='dove'
+
+elsif player1='locke' && player2='hawk'
+
+elsif player1='hawk' && player2='dove'
+
+elsif player1='dove' && player2='hawk'
+
+elsif player1='locke' && player2='locke'
+  
+end
